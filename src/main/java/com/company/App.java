259ab4e8fff93d7d7ce54a3212af37bd75d370a0
calculator <dnltktsnv2004@gmail.com>
2022-10-13 19:20:35 +0300

    1commit

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -5,20 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
 
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Animal animal = context.getBean("myAnimal", Cat.class);
-//        animal.animalPlus();
-//        animal.animalMinus();
-
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Animal animal = context.getBean("myAnimal", Cat.class);
+        animal.animalPlus();
+        animal.animalMinus();
 
         ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Cat cat = context1.getBean("Cats", Cat.class);
+        Cat cat = context1.getBean("Cat", Cat.class);
         System.out.println(cat.getName() + " " + cat.getAge() + " ");
         System.out.println(cat.getBreed() + " " + cat.getColor());
 
-//        ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Person person = context2.getBean("Person", Person.class);
-//        System.out.println(person.getName() + " " + person.getAge());
+        ClassPathXmlApplicationContext context3 = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = context3.getBean("Person", Person.class);
+        System.out.println(person.getName() + " " + person.getAge());
 
     }
 }
